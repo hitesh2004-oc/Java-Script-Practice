1923125da0b7259135f8151b7a1c7caf052f3e42
Hitesh Malviya <hiteshmalviya955@gmail.com>
2025-12-29 12:51:58 +0530

    Practice Session

---
+++ Day1.js
@@ -0,0 +1,56 @@
+// let a = 5;
+// let b = a;
+// b = 20;
+// let d = { name: "John", Age: 20, gmail: "hitesh@" };
+// let e = d;
+// let arr = [1, "hi", 3, 4];
+// e.name = "Doe";
+
+// console.log(d);
+// console.log(e);
+// console.log(arr[1]);
+
+// let a = 30;
+// let b = "ok";
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a / b);
+// console.log(a * b);
+// console.log(a % b);
+
+// if(typeof a == "string"){
+//     console.log("a is greater");
+// }
+// let a = 7;
+// let b = "8";
+
+//   console.log(a == b);
+//   console.log(a === b);
+//   console.log(a != b);
+//   console.log(a !== b);
+//   console.log(a < b);
+//   console.log(a > b);
+//   console.log(a <= b);
+//   console.log(a >= b);
+// let age = 38;
+// console.log(age > 18 && age < 60);
+// console.log(age > 18 || age < 35);
+// console.log(!(age > 18));
+
+// console.log((age > 18 && age < 60)? "Age is valid": "Age is not valid");
+
+// let a = 10;
+// console.log(a++);
+// console.log(++a);
+// console.log(a--);
+// console.log(--a);
+
+// let a = "1234";
+//     a = +"1234";
+//     console.log(a);
+//     b = Number("1234");
+//     console.log(typeof b);
+//
+// let a = 10;
+// let b = 50;
+// let c = 30;
