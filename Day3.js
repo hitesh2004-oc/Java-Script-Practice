d99030d7bf034dea31020812a77ea7d6c3f5aff3
Hitesh Malviya <hiteshmalviya955@gmail.com>
2025-12-30 18:48:26 +0530

    Practice Session

---
+++ Day3.js
@@ -0,0 +1,43 @@
+// for(let i = 1; i<=50;i++){
+//     if(i>0 && 20 >= i || 41 < i || (i > 25 && i < 39)){
+//         console.log(i);
+//     }
+// }
+
+// let index = "yghagsjhasx"
+// let a = index.slice(1,2)
+
+// console.log(a)
+
+// let string = "heLLLohi"
+// let value = "  jdscda        "
+// let b = 'a';
+
+// console.log(string.length);
+// console.log(string.charAt(0));
+// console.log(string.toUpperCase());
+// console.log(string.toLowerCase());
+// console.log(value.trim());
+// console.log(value.trimStart());
+// console.log(value.trimEnd());
+
+// let c = "15";
+// let value = "hhhye";
+// console.log(c.padEnd(7,"0"));
+
+// console.log(c.repeat(8));
+// console.log(value.replaceAll("h","b"));
+// console.log(value.split(''));
+
+// let a = "1234";
+// console.log(a.indexOf("1"));
+
+// console.log(a.lastIndexOf("a"));
+
+// console.log(a.match("p"))
+// console.log(a.matchAll("s"))
+// console.log(a.includes("script") )
+// console.log(a.startsWith("script",5));
+// console.log(a.charAt())
+// console.log(10 + "10")
+//  a = Number("1234");
