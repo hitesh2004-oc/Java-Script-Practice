dc988f6d026b267c2ccdc4af7604d64e839928e7
Hitesh Malviya <hiteshmalviya955@gmail.com>
2025-12-30 18:46:44 +0530

    Practice Session

---
+++ Day2.js
@@ -0,0 +1,62 @@
+// if (a > b && a > c) {
+//   console.log("a is greater");
+// } else if (b > a && b > c) console.log("b is greater");
+// else if (c > a && c > b) {
+//   console.log("c is greater");
+// }
+
+// let opt = 2;
+
+// switch (opt) {
+//   case 1:
+//     comsole.log("a is greater");
+//     break;
+//   case 2:
+//     console.log("b is greater");
+//     break;
+//      case 3:
+//     console.log("c is greater");
+//     break;
+//     default:
+//     console.log("Invalid option");
+// }
+// let username = "hitesh@123";
+// let password = "hitesh123";
+
+// let grade = 90;
+// switch (true) {
+//   case grade >= 90:
+//     console.log("A grade");
+//     break;
+//   case grade >= 80:
+//     console.log("B grade");
+//     break;
+//   case grade >= 70:
+//     console.log("C grade");
+//     break;
+//   case grade >= 60:
+//     console.log("D grade");
+//     break;
+//   case grade >= 50:
+//     console.log("E grade");
+//     break;
+//     default:
+//     console.log("Fail");
+
+// }
+
+// let grade = 80;
+
+// console.log((grade >= 90)? "A grade":
+// (grade >=80)? "B grade":
+// (grade >=70)? "C grade":
+// (grade >=60)? "D grade":
+// (grade >=50)? "E grade": "Fail");
+
+// let mark = 80;
+// let g = (mark > 90)? "A":
+// (mark > 80)? "B":
+// (mark > 70)? "C":
+// (mark > 60)? "D":
+// (mark > 50)? "E": "Fail";
+// console.log(g);
