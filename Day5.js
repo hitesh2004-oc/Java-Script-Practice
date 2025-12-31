4e19e7f9bd820df7cc083826172508be6c94f06b
Hitesh Malviya <hiteshmalviya955@gmail.com>
2025-12-31 13:23:56 +0530

    Practice Session

---
+++ Day5.js
@@ -0,0 +1,46 @@
+// let s = "JavaScripT";
+// let count = 0;
+// for(let i = 0;i<s.length;i++){
+//     if(s[i] == "J" ){
+//         count++;
+//     }
+// }
+// console.log(count);
+
+// let a= 25;
+// let b = 3;
+// let count = 0;
+// for(b = 3;b<=a;b+=3){
+//     count++;
+// }
+// console.log(a-b+3);
+// console.log(count);
+
+// let a = 10;
+// let b = ++a + a++;
+// console.log(b);
+
+// let n = 16;
+// let factor = 0;
+// for (let i = 1; i <= n; i++) {
+//   if (n % i == 0) {
+//     factor++;
+//   }
+// }
+
+// if (factor == 2) {
+//   console.log("Prime");
+// } else console.log("Not Prime");
+
+// let a = 121;
+// let rev = 0;
+// let t = a;
+// while(a > 0){
+//      let d = a%10;
+//      rev = rev*10 + d;
+//      a = (a-d)/10;
+// }
+// console.log(rev);
+// if(rev == t ){
+//     console.log("Palindrome");
+// }else console.log("Not Palindrome");
