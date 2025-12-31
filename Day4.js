06eb4a85d59fa82d2934cb3ebc0dbd1ea6623c01
Hitesh Malviya <hiteshmalviya955@gmail.com>
2025-12-31 13:23:28 +0530

    Practice Session

---
+++ Day4.js
@@ -0,0 +1,45 @@
+//  let b = "hye";
+//  let number = 123.773
+// let a = 27818718731
+// console.log(number.toFixed(2));
+// let d = "hye 100 100";
+// console.log(isNaN(d));
+// console.log(parseInt(number));
+
+// console.log(parseFloat(d));
+
+// console.log(Number.isSafeInteger(10654));
+// -+2^53-1
+
+// let a = "helloworld"
+// let s = 0;
+// let e = a.length-1;
+
+// for(let i = e; i>0;i--){
+// s = s+ char(i);
+//
+// }
+// console.log(s);
+
+// let string = "javascript";
+// // let b = string.toUpperCase.apply("j");
+// // console.log(string.charAt(0).toUpperCase())
+// let count = 0;
+// for(let i = 0; i<string.length;i++){
+//     if(!(string.charAt(i) == "a" || string.charAt(i) == "e" || string.charAt(i) == "i" || string.charAt(i) == "o" || string.charAt(i) == "u" )){
+//       count++;
+//     }
+// }
+// console.log(count);
+// console.log(typeof undefined);
+// console.log(0 == " ");
+// let a = " ";
+// console.log(a.length);
+// console.log("a" + "b" + + "a" + "b");
+// console.log("hi" || 0);
+// console.log()
+// let a = prompt("Enter name");
+// alert(a);
+
+// console.log(a);
+// document.write(a);
