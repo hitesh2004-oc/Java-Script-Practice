bcbc14947a56007a7e6d64d2661e4ba3c4bd2020
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-01-07 21:24:08 +0530

    Practice Session

---
+++ Day13.js
@@ -0,0 +1,38 @@
+// let obj = {
+//     name : "hitesh",
+//     age : 23
+// }
+// console.log(obj);
+
+// let obj = new Object();
+
+// obj.name = "hitesh";
+// obj.name = "vinod"
+// obj.age = 34;
+// console.log(obj);
+
+// let arr = new Array(1,2,3,4,5);
+
+// let obj = new Object();
+// obj["role"] = "Trainer";
+// console.log(obj);
+
+// delete obj.role;
+// console.log(obj);
+
+// let arr = [1,2,3,4,5,6]
+
+// // for of
+// for(let a of arr){
+//     console.log(a);
+// }
+// // for each
+// arr.forEach((el) => console.log(el));
+
+// console.log({} == {});
+// console.log([] == []);
+// console.log(typeof[]);
+
+
+
+
