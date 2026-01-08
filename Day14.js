a5d4888c9f50cbe60560170e0dad69b91175a87f
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-01-08 11:56:02 +0530

    Practice Session

---
+++ Day14.js
@@ -0,0 +1,69 @@
+// let obj = {};
+// obj.name = "abc"
+// obj.age = 65;
+// console.log(obj)
+
+// Using Constructor Function Create Object
+// function user(name,age){
+//     this.name1 = name;
+//     this.age1 = age;
+// }
+
+// let a = new user("hitesh",22)
+// console.log(a);
+
+// Objet Methods[Function Inside Object]
+// const user = {
+//     name: "abc",
+//     xyz: function (){
+//         return this.name;
+//     }
+// }
+// console.log(user.xyz());
+
+// const user = {
+//     name: "abc",
+//     xyz:() => {
+//         return this.name;
+//     }
+// }
+// console.log(user.xyz());
+
+// Object Using For In Loop
+// let obj = {
+//     name:"abc",
+//     age:25,
+//     email:"abc@gmail.com",
+//     contact:9224431346
+// }
+
+// for(let key in obj){
+//     console.log(key, obj[key]); 
+// }
+
+//Return in Array[All Key in Object]
+// let a = Object.keys(obj);
+// console.log(a);
+
+// let b = Object.keys(obj).length;
+// console.log(b);
+
+// let c = Object.values(obj).length;
+// console.log(c);
+
+// let d = Object.values(obj);
+// console.log(d);
+
+// let e = Object.entries(obj);
+// console.log(e);
+
+// let obj = {
+//     name: "hitesh",
+//     arr: ["java","Js"]
+// }
+// console.log(obj.arr[0])
+
+
+
+
+
